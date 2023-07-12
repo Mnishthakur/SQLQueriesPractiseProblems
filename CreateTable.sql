63c12d4acf95bc9e5933d4e4f609a1706fc85e55
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-07-12 23:06:15 +0530

    [Manish Thakur]: Customer data added successfully

--- CreateTable.sql
+++ CreateTable.sql
@@ -4,3 +4,13 @@ CREATE TABLE CustomerInfo.Customer (
   Phone VARCHAR(20),
   Address VARCHAR(255)
 );
+
+-- Insert customer details
+INSERT INTO CustomerInfo.Customer (CustomerName, Phone, Address)
+VALUES ('Harry', '1234567890', '123 Main Street');
+
+INSERT INTO CustomerInfo.Customer (CustomerName, Phone, Address)
+VALUES ('Shyam', '9876543210', '456 Elm Street');
+
+INSERT INTO CustomerInfo.Customer (CustomerName, Phone, Address)
+VALUES ('Manish', '5555555555', '789 Oak Avenue');
